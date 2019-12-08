84504487082c0e37fe22d494c7a46e9ae42cb815
genriber <58648674+genriber@users.noreply.github.com>
2019-12-08 10:44:19 +0400

    Update izmkurs.cs

--- zVisConverter/zVisConverter/izmkurs.cs
+++ zVisConverter/zVisConverter/izmkurs.cs
@@ -34,7 +34,7 @@ namespace zVisConverter
             using (OpenFileDialog openFileDialog1 = new OpenFileDialog())
             {
                 openFileDialog1.InitialDirectory = "c:\\";
-                openFileDialog1.Filter = "Текстовые файлы (*.txt)|*.txt|";
+                openFileDialog1.Filter = "Текстовые файлы (*.txt)|*.txt";
                 openFileDialog1.FilterIndex = 2;
                 openFileDialog1.RestoreDirectory = true;
                 if (openFileDialog1.ShowDialog() == DialogResult.OK)
